433d991676ff4d934856bffad53c6a5fdaf408d7
Madina <madinatussupova0820@gmail.com>
2022-06-09 10:46:45 +0600

    resetting the game

--- app.js
+++ app.js
@@ -15,15 +15,29 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  document.querySelector(".current-score").textContent = "0";
+
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+
+  diceEl.classList.add("hidden");
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -33,7 +47,7 @@ const switchPlayer = function () {
   player1El.classList.toggle("player--active");
 };
 
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     // Generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -53,7 +67,7 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
     // Add curent score to the active player's score
     // scores[i] = scores[i] + currentScore;
@@ -64,7 +78,7 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
-						diceEl.classList.add('hidden');
+      diceEl.classList.add("hidden");
 
       document
         .querySelector(`.player--${activePlayer}`)
@@ -79,6 +93,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-	
-})
+btnNew.addEventListener("click", init);
